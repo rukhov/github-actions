b5f1121854f5c73d809a150ae502eac2e01e5d3c
Roman Ukhov <roman.ukhov@virtru.com>
2024-01-26 20:09:42 +0200

    update

--- 1.resolve.dependencies.cmd
+++ 1.resolve.dependencies.cmd
@@ -1,6 +1,8 @@
 
 ::conan install . --output-folder=build/conan --build=missing
 
+conan profile detect --force
+
 set CUR_DIR=%CD%
 
 conan install . --output-folder=%CUR_DIR%/build/conan --build=missing -s build_type=RelWithDebInfo
