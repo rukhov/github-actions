401bde3067d603f56ba8716a14d8e7066c33ad64
Roman Ukhov <roman.ukhov@virtru.com>
2024-01-26 14:01:28 +0200

    add scripts

---
+++ 2.build.all.cmd
@@ -0,0 +1,8 @@
+
+cmake --build .\build --config "Debug"
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cmake --build .\build --config "RelWithDebInfo"
+
+if %errorlevel% neq 0 exit /b %errorlevel%
