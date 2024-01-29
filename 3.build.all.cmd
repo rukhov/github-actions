d340401bd873bb26a2b122b8459f59365c132829
Roman Ukhov <roman.ukhov@virtru.com>
2024-01-29 13:03:29 +0200

    try cache

--- 3.build.all.cmd
+++ 3.build.all.cmd
@@ -2,5 +2,5 @@
 cmake --build .\build --config "Debug"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-::cmake --build .\build --config "RelWithDebInfo"
-::if %errorlevel% neq 0 exit /b %errorlevel%
+cmake --build .\build --config "RelWithDebInfo"
+if %errorlevel% neq 0 exit /b %errorlevel%
